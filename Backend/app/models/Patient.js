f698273b388abeece9820a3e6ea68669599ba358
bvelasqo <brandon.velasquez.osorio@gmail.com>
2022-02-11 18:54:29 -0500

    Class created

---
+++ Backend/app/models/Patient.js
@@ -0,0 +1,9 @@
+class Patient {
+    constructor(id, name, phone_number, email) {
+        this.id = id;
+        this.name = name;
+        this.phone_number = phone_number;
+        this.email = email;
+    }
+}
+module.exports = Patient;
